30c7f05e7ee6d66df18fb1f98d3f74e97f79ac7c
jideabdqudus <jideabdqudus@gmail.com>
2020-05-10 17:26:00 +0100

    SEO Creation

--- src/sections/SEO.js
+++ src/sections/SEO.js
@@ -1,74 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
 
-
-function Seo({ description, lang, meta, title, slug, siteImage }) {
-    const { site } = useStaticQuery(
-        graphql`
-        query {
-            site {
-                siteMetadata {
-                    description
-                    author
-                    siteUrl
-                    siteImage
-                }
-            }
-        }
-        `
-    );
-
-    const metaDescription = description || site.siteMetadata.description;
-
+function SEO() {
     return (
-        <Helmet
-        htmlAttributes={{
-            lang,
-          }}
-          title={title}
-          meta={[
-            {
-               name: `description`,
-               content: metaDescription,
-             },
-             {
-               property: `og:title`,
-               content: title,
-             },
-             {
-               property: `og:description`,
-               content: metaDescription,
-             },
-             {
-               property: `og:type`,
-               content: `website`,
-             },
-             {
-               name: `twitter:card`,
-               content: `summary_large_image`,
-             },
-             {
-               name: `twitter:creator`,
-               content: `${site.siteMetadata.author}`,
-             },
-             {
-                name: `twitter:image`,
-                content: `${site.siteMetadata.siteImage}`,
-            },
-             {
-               name: `twitter:title`,
-               content: title,
-             },
-             {
-               name: `twitter:description`,
-               content: metaDescription,
-             }
-          ].concat(meta)}
-        />
+        <div>
+            
+        </div>
     )
 }
 
-export default Seo;
+export default SEO
