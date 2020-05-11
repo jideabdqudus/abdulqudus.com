324f82cd14d7f5fa1a853dc93088c21b74f476e1
jideabdqudus <jideabdqudus@gmail.com>
2020-05-11 17:36:52 +0100

    SEO Creation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
-import Writing from '../sections/Writing';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Writing from "../sections/Writing"
 
 const IndexPage = () => (
   <Layout>
